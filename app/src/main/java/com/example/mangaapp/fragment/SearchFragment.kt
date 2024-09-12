a6eb85054249d4b230061082a7a0b3e7e0b1c5d7
MuTofu <57164519+MuTofu@users.noreply.github.com>
2024-09-12 09:26:07 +0700

    Fix Toolbar

--- app/src/main/java/com/example/mangaapp/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/mangaapp/fragment/SearchFragment.kt
@@ -2,6 +2,7 @@ package com.example.mangaapp.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,6 +24,8 @@ class SearchFragment : Fragment(), SearchResultAdapter.RecyclerViewEvent {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_search, container, false)
+
+        Log.e("refresing", "onCreateView: Refresh", )
 
         homedata = ArrayList<HomeDummyData>()
 
